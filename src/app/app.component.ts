8b4f6ba25e62b479dcb696952bb4132713506f9a
Gregory Heller <gdheller@gmail.com>
2026-02-04 13:46:26 -0800

    added  create account/login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,21 @@ import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
+import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
 
-Amplify.configure(outputs);
+//Amplify.configure(outputs);
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, TodosComponent],
+    imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule],
 })
 export class AppComponent {
   title = 'amplify-angular-template';
+    
+  constructor(public authenticator: AuthenticatorService) {
+    Amplify.configure(outputs);
+  }
 }
